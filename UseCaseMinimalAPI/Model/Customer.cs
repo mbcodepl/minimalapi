7260728b8e5eaaf24e78d0ece081db02bc8623c7
untitledsoft <mattix19@gmail.com>
2023-01-16 13:00:23 +0100

    nullable

--- UseCaseMinimalAPI/Model/Customer.cs
+++ UseCaseMinimalAPI/Model/Customer.cs
@@ -3,6 +3,6 @@ namespace UseCaseMinimalAPI.Model;
 public class Customer
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public DateTimeOffset DateAdd { get; set; }
 }
